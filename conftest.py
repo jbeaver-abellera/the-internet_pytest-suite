bc51ab9fbe89d10eec55467ecd08c7d0f74f0209
jbeaver-abellera <abellera.beaver@gmail.com>
2024-06-28 12:53:41 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,14 +1,1 @@
-# import utils
-# import pytest
-
-
-# @pytest.fixture(scope='session')
-# def suite_conftest():
-#     driver = utils.WebDriverSingleton.get_driver(isHeadless=True)
-#     print('conftest setup completed')
-    
-#     yield driver
-    
-#     print('teardown from conftest complete')
-#     driver.quit()
 
